dc3f3242cbc91447204ec5501dc3fc7573df15f9
Ljubica Smiljic <lj.smiljic@gmail.com>
2025-05-10 20:14:02 +0200

    Ispravka gresaka

--- music-app-frontend/src/components/Breadcrumbs.jsx
+++ music-app-frontend/src/components/Breadcrumbs.jsx
@@ -6,6 +6,21 @@ const Breadcrumbs = () => {
   const location = useLocation();
 
   // Delimo putanju po '/' i uklanjamo eventualne prazne segmente
+  // link koji ste uzeli http://localhost:3000/home i to je location konstanta
+  // kada uradite location.pathname tada dobijate "/home"
+  // split('/') - vas string '/home' postaje niz stringova razdvojnih po karakteru '/'
+  // dakle dobijate niz ['', 'home']
+  // sada se poziva filter funkcija
+  // falsy values -> 0, '', null, undefined, false, NaN
+  // truthy values -> sve sto nije falsy
+  // filter ona izbacuje sve elemente iz niza koji nisu truthy
+  // dakle nece se ispisati ''
+  // dakle dobijamo ['home']
+
+  //Drugi primer je link http://localhost:3000/events/3
+  // location.pathname je '/events/3'
+  // split('/') -> ['','events', '3']
+  // filter -> ['events', '3']
   const pathnames = location.pathname.split('/').filter(x => x);
   
   // Ne prikazujemo breadcrumbs na root ("/") ili stranici za prijavu ("/auth")
@@ -20,6 +35,8 @@ const Breadcrumbs = () => {
       
       {pathnames.map((name, index) => {
         // Rekonstruišemo URL za trenutni deo putanje
+        // '/events/'
+        // '/3/'
         const routeTo = `/${pathnames.slice(0, index + 1).join('/')}`;
         // Da li je ovo poslednja stavka u breadcrumbs?
         const isLast = index === pathnames.length - 1;
